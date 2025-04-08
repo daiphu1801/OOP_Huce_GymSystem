ed3a9c8d2b04dcfa6349608e331642f7e1a274cb
VAK <buidaiphu8@gmail.com>
2025-04-08 23:00:43 +0700

    database

--- src/main/java/com/gym/oop_huce_gymsystem/util/DatabaseConnection.java
+++ src/main/java/com/gym/oop_huce_gymsystem/util/DatabaseConnection.java
@@ -1,21 +1,19 @@
 package com.gym.oop_huce_gymsystem.util;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
 public class DatabaseConnection {
-    private static final String JDBC_URL = "jdbc:sqlserver://localhost:1433;databaseName=GymManagement";
-    private static final String USER = "sa";  // Hoặc user bạn đặt
-    private static final String PASSWORD = "123"; // Đổi thành mật khẩu của bạn
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/gymmanagement?useSSL=false&serverTimezone=UTC";
+    private static final String USER = "root";       // Thay đổi thành tài khoản của bạn
+    private static final String PASSWORD = "123456"; // Thay đổi thành mật khẩu của bạn
 
     public static Connection getConnection() throws SQLException {
         try {
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
-            System.out.println("Không tìm thấy JDBC Driver.");
+            System.out.println("Không tìm thấy JDBC Driver cho MySQL.");
             e.printStackTrace();
         }
 
